b2d0bf12694111a266a7a182dbae02405724d944
Samson <31880780+Zerueru90@users.noreply.github.com>
2021-08-27 10:43:43 +0200

    fin

--- js/bishbosh.js
+++ js/bishbosh.js
@@ -1,40 +1,46 @@
-function rangeValue(range, label){
+function rangeValue(range, label)
+{
     document.getElementById(label).innerHTML = document.getElementById(range).value
 }
 
-function getValue(){
+function getValue()
+{
     var rangeNumber = document.getElementById("nummer").value;
     var bishNumber = document.getElementById("bish").value;
     var boshNumber = document.getElementById("bosh").value;
-    var bishboshlist = document.getElementById("bish-bosh-list");
+    var bishboshlist = document.getElementById("bishbosh-grid");
 
     bishboshlist.innerHTML = "";
 
-    for(let i = 1; i <= rangeNumber; i++){
+    for(let i = 1; i <= rangeNumber; i++)
+    {
 
-        var item = document.createElement("li");
-
+        var item = document.createElement("div");
+        item.style.width = "100px";
+        item.style.height = "25px";
+        item.style.background = "grey";
+        bishboshlist.appendChild(item);
+        
         if(i % bishNumber == 0 && i % boshNumber == 0){
 
             item.innerHTML = "Bish-Bosh";
-            // item.className = "green";
+            item.style.backgroundColor = "green";
             bishboshlist.appendChild(item);
 
         }
         else if(i % bishNumber == 0){
             item.innerHTML = "Bish";
-            // item.className = "red";
+            item.style.backgroundColor = "red";
             bishboshlist.appendChild(item);
         }
         else if(i % boshNumber == 0){
             item.innerHTML = "Bosh";
-            // item.className = "blue";
+            item.style.backgroundColor = "blue";
             bishboshlist.appendChild(item);
         }
         else{
             item.innerHTML = i;
             bishboshlist.appendChild(item);
         }
-
     }
 }
